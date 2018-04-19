f0d04157cbd327cc6d841c6b541d99693e103e7e
Piboxy <admin@piboxy.com>
2018-04-18 23:17:22 -0400

    fix recalbox beta script

--- install_script.sh
+++ install_script.sh
@@ -1,104 +1,45 @@
 #!/bin/bash
-#
+#Step1) remount boot and root partition make writable.
+mount -o remount, rw /boot
+mount -o remount, rw /
 
-hostname=$(hostname)
-if [[ "$hostname" == "BATOCERA" ]];then
+#Step 3) Download Python script
+mkdir -p  /opt/PiBoxy
+sleep 2s
 
-echo "Installing BATOCERA Piboxy Driver..."
+script=/opt/PiBoxy/recalbox_SafeShutdown.py
 
-gpio readall > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then 
-
-mkdir -p /recalbox/share/system/bin/
-curl http://piboxy.com/download/remoteswitch > /recalbox/share/system/bin/remoteswitch
-chmod a+x /recalbox/share/system/bin/remoteswitch
-echo "/recalbox/share/system/bin/remoteswitch -r 17 -s 18 -n 4 &" > /recalbox/share/system/custom.sh
-chmod a+x /recalbox/share/system/custom.sh
-
-mount -o remount -rw /boot
-
-if grep -q "^dtparam=i2c_arm=off" /boot/config.txt; then
-	sed -i "s|^dtparam=i2c_arm=off|dtparam=i2c_arm=on|" /boot/config.txt &> /dev/null
-elif grep -q "^#dtparam=i2c_arm=on" /boot/config.txt; then
-	sed -i "s|^#dtparam=i2c_arm=on|dtparam=i2c_arm=on|" /boot/config.txt &> /dev/null
-else
-	echo "dtparam=i2c1=on" | tee -a /boot/config.txt &> /dev/null
-	echo "dtparam=i2c_arm=on" | tee -a /boot/config.txt &> /dev/null
+if [ -e  $script ];
+        then              
+        else
+                wget --no-check-certificate -O  $script "https://raw.githubusercontent.com/PiboxyTeam/remoteswitch/recalbox/recalbox_SafeShutdown.py"
 fi
+#-----------------------------------------------------------
 
+#Step 4) Enable Python script to run on start up------------
+DIR=/etc/init.d/S99PiBoxy
 
-if grep -q "^vc_i2c_override=0" /boot/cmdline.txt; then
-	sed -i "s|^vc_i2c_override=0|vc_i2c_override=1|" /boot/cmdline.txt &> /dev/null
-else
-	echo -n "bcm2708.vc_i2c_override=1" >> /boot/cmdline.txt
+if grep -q "python $script &" "S99PiBoxy";
+        then
+                if [ -x $DIR];
+                        then 
+                                echo "Executable S99PiBoxy already configured. Do nothing."
+                        else
+                                chmod +x $DIR
+                fi
+        else
+                echo "python $script &" >> $DIR
+                chmod +x $DIR
+                echo "Executable S99PiBoxy configured."
 fi
-sync
-mount -o remount -r /boot
+#-----------------------------------------------------------
 
-if ! [ -e /etc/modprobe.d/raspi-blacklist.conf ]; then
-touch /etc/modprobe.d/raspi-blacklist.conf
-fi
-sed /etc/modules.conf -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
-if ! grep -q "^i2c[-_]dev" /etc/modules.conf ; then
-printf "i2c-dev\n" >> /etc/modules.conf
-printf "i2c_bcm2708\n" >> /etc/modules.conf
-fi
+#Step 5) Reboot to apply changes----------------------------
+echo "PiBoxy Remote Power Switch Driver's installation is accomplished . Will now reboot after  5 seconds."
+for  i in `seq 0 5 |sort -rn `;
+ do 
+    echo  "$i  Sec"
+    sleep  1
+done  
+shutdown -r now
 
-/recalbox/scripts/recalbox-save-overlay.sh
-
-modprobe i2c-dev
-modprobe i2c_bcm2708
-sync
-/recalbox/share/system/bin/remoteswitch -r 17 -s 18 -n 4 &
-
-echo "Rebooting to finish installation"
-sync
-sleep 2
-reboot
-else 
-
-echo "Your batocera wiringpi can not detect your Raspberry Pi model, if you think it is a BUG, please contact us.";
-echo "=================================================="
-gpio readall
-echo "=================================================="
-exit
-
-fi
-
-else
-
-echo "Installing Piboxy Driver..."
-
-sudo apt-get update
-sudo apt-get install -y -qq wiringpi
-sudo sh -c " curl http://piboxy.com/download/remoteswitch > /bin/remoteswitch "
-sudo chmod a+x /bin/remoteswitch
-sudo sed -i '$i sudo remoteswitch -r 17 -s 18 -n 4 >> /tmp/piboxy.log &' /etc/rc.local
-sudo sync
-
-#Enable I2C
-
-if grep -q "^dtparam=i2c_arm=off" /boot/config.txt; then
-	sudo sed -i "s|^dtparam=i2c_arm=off|dtparam=i2c_arm=on|" /boot/config.txt &> /dev/null
-elif grep -q "^#dtparam=i2c_arm=on" /boot/config.txt; then
-	sudo sed -i "s|^#dtparam=i2c_arm=on|dtparam=i2c_arm=on|" /boot/config.txt &> /dev/null
-else
-	echo "dtparam=i2c_arm=on" | sudo tee -a /boot/config.txt &> /dev/null
-fi
-
-if ! [ -e /etc/modprobe.d/raspi-blacklist.conf ]; then
-sudo touch /etc/modprobe.d/raspi-blacklist.conf
-fi
-sudo sed /etc/modprobe.d/raspi-blacklist.conf -i -e "s/^\(blacklist[[:space:]]*i2c[-_]bcm2708\)/#\1/"
-sudo sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
-if ! grep -q "^i2c[-_]dev" /etc/modules; then
-sudo sh -c "echo i2c-dev >> /etc/modules"
-fi
-sudo dtparam i2c_arm=on
-sudo modprobe i2c-dev
-
-sudo sync
-sudo remoteswitch -r 17 -s 18 -n 4 &
-
-fi
