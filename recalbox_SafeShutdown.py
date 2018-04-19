4ba6a4ad12f22f48d97cba51cd66c534639e4526
PiBoxY <34830462+PiBoxY@users.noreply.github.com>
2018-04-19 11:38:36 +0800

    Update recalbox_SafeShutdown.py

--- recalbox_SafeShutdown.py
+++ recalbox_SafeShutdown.py
@@ -14,19 +14,21 @@ def init():
 	GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(rebootPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(signalPin, GPIO.OUT)
-	GPIO.output(signalPin, GPIO.HIGH)
+	GPIO.output(signalPin, GPIO.LOW)
 	GPIO.setwarnings(False)
 
 #waits for user to hold button up to 1 second before issuing poweroff command
 def poweroff():
 	while True:
 		GPIO.wait_for_edge(shutdownPin, GPIO.FALLING)
+		os.system("sync")
 		os.system("shutdown -h now")
 
 #resets the pi
 def reset():
 	while True:
 		GPIO.wait_for_edge(rebootPin, GPIO.FALLING)
+		os.system("sync")
 		os.system("shutdown -r now")
 
 
