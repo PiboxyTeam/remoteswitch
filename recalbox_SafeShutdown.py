8751aef0f9b79dce9b6e0d41754ed3f9ac1ff3a1
PiBoxY <34830462+PiBoxY@users.noreply.github.com>
2018-04-19 11:06:57 +0800

    Create recalbox_SafeShutdown.py

---
+++ recalbox_SafeShutdown.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import os
+import time
+from multiprocessing import Process
+
+#initialize pins
+shutdownPin = 18  #BCM mode    
+rebootPin = 17  
+signalPin = 4  
+
+#initialize GPIO settings
+def init():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(rebootPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(ledPin, GPIO.OUT)
+	GPIO.setup(signalPin, GPIO.OUT)
+	GPIO.output(signalPin, GPIO.HIGH)
+	GPIO.setwarnings(False)
+
+#waits for user to hold button up to 1 second before issuing poweroff command
+def poweroff():
+	while True:
+		GPIO.wait_for_edge(shutdownPin, GPIO.FALLING)
+		os.system("shutdown -h now")
+
+#resets the pi
+def reset():
+	while True:
+		GPIO.wait_for_edge(rebootPin, GPIO.FALLING)
+		os.system("shutdown -r now")
+
+
+if __name__ == "__main__":
+	#initialize GPIO settings
+	init()
+	#create a multiprocessing.Process instance for each function to enable parallelism 
+	powerProcess = Process(target = poweroff)
+	powerProcess.start()
+	resetProcess = Process(target = reset)
+	resetProcess.start()
+
+	powerProcess.join()
+	resetProcess.join()
+
+	GPIO.cleanup()
